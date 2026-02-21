3ac2b3e1c6c818aaaf46a0837df980963c3b04d0
Yusaku-KASAI <kasai.yusaku.work@gmail.com>
2026-02-21 15:57:51 +0900

    fix main

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:intl/date_symbol_data_local.dart'; // 日本語などロケール情報を読み込む
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'screens/list_screen.dart';
 import 'services/todo_service.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   // Flutter のプラグイン初期化。非同期処理を行う場合は必須
   WidgetsFlutterBinding.ensureInitialized();
 
-  // ❗️ SharedPreferences のインスタンスを作成してみましょう
+  // TodoCard内で DateFormat(..., 'ja') を使うので、起動時に一度だけ初期化しよう
+  await initializeDateFormatting('ja'); // 英語表記なら "en" など
+
+  // ① 端末保存（SharedPreferences）を使えるようにしよう
   final prefs = await SharedPreferences.getInstance();
 
-  // ❗️ 作成した prefs を引数として TodoService のインスタンスを作成してみましょう
+  // ② prefs を渡して TodoService を作り、保存/読み込みの窓口を1つにまとめよう
   final todoService = TodoService(prefs);
 
-  runApp(
-    MyApp(
-      // ❗️ 最後にMyAppへtodoServiceを引数として渡してみましょう
-      todoService: todoService,
-    ),
-  );
+  // ③ todoService をアプリ全体へ渡して、どの画面からでも使えるようにしよう
+  runApp(MyApp(todoService: todoService));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({
-    super.key,
-    required this.todoService, // 引数として TodoService を受け取るようにします（required は引数として必須であることを示すキーワードです）
-  });
+  const MyApp({super.key, required this.todoService});
 
-  // アプリ全体で共有する TodoService を引数として受け取るために変数として定義します
+  // アプリ全体で共有する TodoService
   final TodoService todoService;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ListScreen(
-        // ❗️ ListScreen へ todoService を引数としてわたしてみましょう
-        todoService: todoService,
-      ),
+      // ListScreen にtodoServiceを引数としてわたす
+      home: ListScreen(todoService: todoService),
     );
   }
 }
