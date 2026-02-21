4d3af1ed7722dd9cccda8bac04229cedca6a86d7
Yusaku-KASAI <kasai.yusaku.work@gmail.com>
2026-02-21 16:20:23 +0900

    fix list widget

--- lib/widgets/todo_list.dart
+++ lib/widgets/todo_list.dart
@@ -5,14 +5,9 @@ import '../services/todo_service.dart'; // データ保存サービス
 import '../widgets/todo_card.dart'; // 作成したTodoCardウィジェット
 
 class TodoList extends StatefulWidget {
-  const TodoList({
-    super.key,
-    // ❗️ 引数としてtodoServiceを受け取るようにしましょう（必須であることを示す required を忘れずに！）
-    required this.todoService,
-  });
+  const TodoList({super.key, required this.todoService});
 
-  // ❗️ ListScreen で利用する TodoService を引数として受け取るために変数として定義しましょう
-  final TodoService todoService;
+  final TodoService todoService; // getTodos/saveTodos を呼べるように受け取ろう
 
   @override
   State<TodoList> createState() => TodoListState();
@@ -20,19 +15,16 @@ class TodoList extends StatefulWidget {
 
 class TodoListState extends State<TodoList> {
   List<Todo> _todos = [];
-  // ❗️ 読み込み中であることを示すフラグ _isLoading を変数として定義しましょう
-  // 画面表示時は読み込み中であることを示すために true を代入しましょう
   bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    // ❗️ Todoリストのデータを読み込むため、TodoListState で定義した _loadTodos() を呼び出しましょう
-    _loadTodos();
+    _loadTodos(); // 起動時に端末から読み込んで表示しよう
   }
 
-  // データ読み込み処理関数を追加
   Future<void> _loadTodos() async {
+    // 端末保存から読み込んで、画面に反映しよう
     final todos = await widget.todoService.getTodos();
     setState(() {
       _todos = todos;
@@ -40,13 +32,13 @@ class TodoListState extends State<TodoList> {
     });
   }
 
-  // 追加画面から呼ばれる追加関数を追加
+  // 追加画面から呼ばれる：リストに追加して保存まで行おう
   void addTodo(Todo newTodo) async {
     setState(() => _todos.add(newTodo));
     await widget.todoService.saveTodos(_todos);
   }
 
-  // チェックボタンから呼ばれる削除関数を追加
+  // チェック（完了）ボタンから呼ばれる：削除して保存まで行おう
   Future<void> _deleteTodo(Todo todo) async {
     setState(() => _todos.removeWhere((t) => t.id == todo.id));
     await widget.todoService.saveTodos(_todos);
@@ -55,22 +47,20 @@ class TodoListState extends State<TodoList> {
   @override
   Widget build(BuildContext context) {
     if (_isLoading) {
-      // 読込中はローディングインジケーターを表示
-      return const Center(
-        child:
-            // ❗️ CircularProgressIndicator を表示してみましょう
-            CircularProgressIndicator(),
-      );
+      // 読み込み中はローディング表示にしよう
+      return const Center(child: CircularProgressIndicator());
     }
+
     return ListView.builder(
       itemCount: _todos.length,
       itemBuilder: (context, index) {
-        final todo = _todos[index]; // ←追加
+        final todo = _todos[index];
         return Padding(
           padding: const EdgeInsets.all(8.0),
           child: TodoCard(
             todo: todo,
-            onToggle: () => _deleteTodo(todo), // チェックで削除する処理を追加
+            // チェックボタンを押したら _deleteTodo が呼ばれるように渡そう
+            onToggle: () => _deleteTodo(todo),
           ),
         );
       },
