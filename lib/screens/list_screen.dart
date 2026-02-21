8be79254ccd2a294b63cc27b49fca24e0e4398ea
Yusaku-KASAI <kasai.yusaku.work@gmail.com>
2026-02-21 16:03:49 +0900

    mod list screen

--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,45 +1,65 @@
 import 'package:flutter/material.dart';
+import '../services/todo_service.dart';
 import '../screens/add_todo_screen.dart';
-
 import '../widgets/todo_list.dart';
 
 class ListScreen extends StatefulWidget {
-  const ListScreen({super.key});
+  const ListScreen({
+    super.key,
+    // ❗️ 引数としてtodoServiceを受け取るようにしましょう（必須であることを示す required を忘れずに！）
+    required this.todoService,
+  });
+
+  // ❗️ ListScreen で利用する TodoService を引数として受け取るために変数として定義しましょう
+  final TodoService todoService;
 
   @override
-  ListScreenState createState() => ListScreenState();
+  State<ListScreen> createState() => ListScreenState();
 }
 
 class ListScreenState extends State<ListScreen> {
-  // TodoList の状態を操作するためのキー
-  final GlobalKey<TodoListState> _todoListKey = GlobalKey<TodoListState>();
+  // ❗️ TodoList の状態を操作するための UniqueKey を変数として定義しましょう
+  // ❗️ ここでは _todoListKey という名前で定義してみましょう
+  Key _todoListKey = UniqueKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('TODOリスト')),
-      body: TodoList(key: _todoListKey), // TodoList ウィジェットを配置
+      body: TodoList(
+        // ❗️ 作成した UniqueKey を引数として渡してみましょう
+        key: _todoListKey,
+        // ❗️ 続いて todoService を引数として渡してみましょう（widget.todoService は引数として受け取った todoService を参照するためのキーワードです）
+        todoService: widget.todoService,
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           // 画面遷移し、戻ってきたら結果（新規 Todo）を受け取る
-          final result = await Navigator.push(
+          final updated = await Navigator.push(
+            // ←追加　Todoに追加があったらtrueを返す
             context,
-            MaterialPageRoute(builder: (context) => const AddTodoScreen()),
+            MaterialPageRoute(
+              builder: (context) => AddTodoScreen(
+                // ❗️ ここでも AddTodoScreen でTodoが追加されたら状態を更新するため、todoService を引数として渡してみましょう
+                todoService: widget.todoService,
+              ),
+            ),
           );
 
-          // 返却されたデータが Todo であればリストへ追加
-          if (result != null) {
-            // 後々使うので一旦コメントアウト
-            // _todoListKey.currentState?.addTodo(result);
+          // 追加があったら再描画（TodoList を再取得）  // ←追加
+          if (updated == true) {
+            setState(() {
+              _todoListKey = UniqueKey(); // 新しいキーで TodoList を再構築
+            });
           }
-        }, // 画面遷移については次の章で実装します
+        },
         backgroundColor: const Color.fromARGB(
           255,
           0,
           0,
           255,
-        ), // ボタン色（RGBAでも指定できます）
-        foregroundColor: Colors.white,
+        ), // ボタンの背景色（RGBAでも指定できます）
+        foregroundColor: Colors.white, // アイコンやテキストなど、ボタン内の要素の色
         child: const Icon(Icons.add), // Flutter標準の「＋」アイコン
       ),
     );
